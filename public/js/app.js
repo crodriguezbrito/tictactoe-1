a9a9451a595d4c5681775cec7829be721c04a99d
Carlos <crodriguezbrito@gmail.com>
2013-12-04 11:51:40 +0000

    Ajax

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,26 @@
-/*
-$(function() {
-  $(".cell").click(function(e) {
-    $.get(e.srcElement.id);
-  });
+$(document).ready(function(){
+    $(".cell").click(function(event) {
+        var pathname = window.location.pathname;
+        var ruta = pathname + event.target.id;
+        $.get(ruta, function(data) {
+            if (data == 'illegal'){
+                alert("Illegal move!");
+                }
+            else if (data.length < 3) {
+                if (data.length > 0){
+                    $("#"+event.target.id).addClass("circle");
+                    $("#"+data).addClass( "cross" );
+                }
+                else{
+                    alert("Casilla Ocupada, Escoja Otra");
+                }
+            }
+            else{ //Redirección a la página de ganar, empate o perder.
+                setTimeout(function(){
+                url = data;
+                $(location).attr('href',url);
+                },200);
+            }
+        });
+    });
 });
-*/
